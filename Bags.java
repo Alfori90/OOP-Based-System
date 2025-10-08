3a5704941bd4db4e215b7b449669b690cda29617
Huria <alforihuria938@gmail.com>
2025-10-08 12:46:36 +0400

    Adding bags and bagDriver class

---
+++ Bags.java
@@ -0,0 +1,38 @@
+public class Bags {
+    private Integer id;
+    private Float price;
+    private String brand;
+    private String size;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
